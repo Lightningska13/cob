6c69420104c9e157d45de20d82c40393887411fc
Alan Elliott <alan@ils-bc.com>
2012-07-13 10:19:38 -0400

    precompiling with ckeditor assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Cob::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  #ADDED FOR CKEDITOR
+  #config.assets.precompile += [ /.*config\.js/, /.*en.js/, /.*editor.css/ ]
+  config.assets.precompile += Ckeditor.assets
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
