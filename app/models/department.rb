496e0f2e37b49f761116c81b5c5633b7d52361ca
Alan Elliott <alan@ils-bc.com>
2012-07-14 20:59:49 -0400

    fixed dept pic

--- app/models/department.rb
+++ app/models/department.rb
@@ -5,7 +5,7 @@ class Department < ActiveRecord::Base
 	has_many :contacts
 	has_and_belongs_to_many :users
   has_attached_file :pic, :styles => { :tiny => "25x25#", :small => "72x72#", :medium => "150x150>"}
-	attr_accessible :dept_fax, :description, :dept_name, :dept_email, :dept_location, :dept_mailing, :dept_phone, :dept_hours
+	attr_accessible :dept_fax, :description, :dept_name, :dept_email, :dept_location, :dept_mailing, :dept_phone, :dept_hours, :pic
 	validates_presence_of :dept_name
 	
 	def self.get_contacts(dept)
