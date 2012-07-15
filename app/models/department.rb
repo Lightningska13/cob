2cf4f2d0310b20db641923366cd44022915265cc
Alan Elliott <alan@ils-bc.com>
2012-07-14 21:20:09 -0400

    more dept pic changes

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,7 +4,7 @@ class Department < ActiveRecord::Base
 	has_and_belongs_to_many :resources
 	has_many :contacts
 	has_and_belongs_to_many :users
-  has_attached_file :pic, :styles => { :tiny => "25x25#", :small => "72x72#", :medium => "150x150>"}
+  has_attached_file :pic, :styles => { :tiny => "25x25#", :small => "72x72#", :medium => "190"}
 	attr_accessible :dept_fax, :description, :dept_name, :dept_email, :dept_location, :dept_mailing, :dept_phone, :dept_hours, :pic
 	validates_presence_of :dept_name
 	
